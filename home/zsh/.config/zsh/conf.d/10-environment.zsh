526dbb912adf7f6e4006720c22aad29f1634d42e
Anthony Portugal <66185678+anthonyportugal@users.noreply.github.com>
2026-09-01 16:36:12 -0500

    feat(desktop): add foot terminal package with catppuccin mocha and dynamic terminal detection

--- home/zsh/.config/zsh/conf.d/10-environment.zsh
+++ home/zsh/.config/zsh/conf.d/10-environment.zsh
@@ -8,3 +8,9 @@ export VISUAL="${VISUAL:-$EDITOR}"
 export BROWSER="${BROWSER:-brave}"
 export BAT_THEME="${BAT_THEME:-Catppuccin-mocha}"
 export STARSHIP_CONFIG="${STARSHIP_CONFIG:-${XDG_CONFIG_HOME:-$HOME/.config}/starship.toml}"
+
+if [[ -n "${WAYLAND_DISPLAY-}" ]]; then
+  export TERMINAL="${TERMINAL:-foot}"
+else
+  export TERMINAL="${TERMINAL:-alacritty}"
+fi
